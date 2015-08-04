f4c797dd8f268cdd23fd34a2beeeef4bdbceef94
Darren Maczka <darmacz@gmail.com>
2015-08-03 21:15:38 -0400

    use superagent instead of jquery

--- util/leapi.js
+++ util/leapi.js
@@ -1,34 +1,32 @@
-var $ = require('jquery');
+var request = require('superagent');
 var config = require('../config.js');
 
 var leapi = {
     auth: function(email, password, fn) {
-        $.ajax(
-            {type: 'POST',
-             url: config.baseUrl+'/token',
-             beforeSend: function(xhr) {
-                 xhr.setRequestHeader('Authorization', 'Basic ' + bota(email + ':' + password));
-             },
-             success: function(response, status) {
-                 fn({token:response.token});
-             },
-             error: function(jqXHR) {
-                 fn({token:null});
-             }
+        request.get(config.baseUrl+'/token')
+	    .withCredentials()
+	    .auth('dmaczka@vt.edu','python')
+            .end(function(err, response) {
+		if (response) {
+                    fn({token:response.token});
+		} else {
+                    fn({token:null});
+		}
             });
     },
     syncNote: function(note,fn) {
-	$.post(apiHost+'/media',note,function(object,status) {
+	request.post(config.baseUrl+'/media',note,function(object,status) {
 	    console.log('status: '+status);
 	    var formData = new FormData();
 	    formData.append('file',note.file);
 	    var imageContent = apiHost+object.href;
-	    $.post(imageContent,formData,function(object,status){
+	    request.post(imageContent,formData,function(object,status){
 		fn(object,status);
 	    });
 	});
     }
 };
+
 
 module.exports = leapi;
 
